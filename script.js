656064d126c3995185bcaf9838b7e46dec239eff
Ruslan <ruslantastanbekov940@gmail.com>
2024-07-16 21:20:03 +0300

    finished commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let userName = "Ruslan";
+let age = "19";
+let total = `${userName} ${age}`;
+console.log(total);
+
+const name = "меня зовуд Руслан";
+const vozrast = "и мня 19 лет";
+const and = name + "" + vozrast;
+console.log(and);
+
+const sushi = "sushi";
+console.log(sushi);
+let numbers = 500;
+console.log(numbers);
+const pizza = " pizza";
+console.log(pizza);
+let boom = 200;
+console.log(boom);
+const totals = 700;
+console.log(totals);
+
+// let info = confirm("вам исполнилось 18 лет");
+// alert(info);
+
+let info = prompt("канча зарплата аласын?");
+
+
